778a6f78ae0744470ee5ec5e4018af0c6bd620ba
marcela <marceg8810@gmail.com>
2021-12-23 23:19:50 -0500

    contador

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
+function App () {
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count+1)
+  }
+  
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{count}</span>
+        <button id="inc" onClick={increment}>Incrementa</button>
       </div>
     );
-  }
 }
 
 export default App;
